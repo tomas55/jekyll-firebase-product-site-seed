2330f960bfa628a61582b8e0cff5fd8a4d5b6daa
Tomas <tomas.vaiciunas@gmail.com>
2019-05-23 13:51:46 +0300

    init from default environment

--- functions/src/config.ts
+++ functions/src/config.ts
@@ -1,10 +1,29 @@
 import * as admin from 'firebase-admin';
-import key from './service-key.json';
+import serviceAccount from './service-key.json';
+// import * as functions from 'firebase-functions';
 
 
-admin.initializeApp({
-    serviceAccountId: key.client_email
-});
+const adminConfig = JSON.parse(process.env.FIREBASE_CONFIG || '{}');
 
+const params = {
+    type: serviceAccount.type,
+    projectId: serviceAccount.project_id,
+    privateKeyId: serviceAccount.private_key_id,
+    privateKey: serviceAccount.private_key,
+    clientEmail: serviceAccount.client_email,
+    clientId: serviceAccount.client_id,
+    authUri: serviceAccount.auth_uri,
+    tokenUri: serviceAccount.token_uri,
+    authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
+    clientC509CertUrl: serviceAccount.client_x509_cert_url
+  }
+
+adminConfig.credential = admin.credential.cert(params);
+
+// admin.initializeApp({
+//     serviceAccountId: key.client_email,
+//     databaseURL: functions.config().db.url
+// });
+admin.initializeApp(adminConfig);
 
 export default admin;
